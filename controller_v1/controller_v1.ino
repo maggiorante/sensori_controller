351334436acd839fe47a22140e37106bafe3cc9a
Xhorxho Papallazi <xho99@hotmail.it>
2023-02-16 17:11:12 +0100

    fix for controller_v1 when removing gravity

--- controller_v1/controller_v1.ino
+++ controller_v1/controller_v1.ino
@@ -1,24 +1,26 @@
 #include "Arduino_LSM9DS1.h"
-#include "MadgwickAHRS.h"
+#include "SensorFusion.h"
 #include "math.h"
 #include "filters.h"
 
 // High pass filter
-const float cutoff_freq = 100.0f;
-const float sampling_time = 0.01f;
+const float cutoff_freq = 200.0f;
+const float sampling_time = 0.00210084f;
 IIR::ORDER order = IIR::ORDER::OD1;
 Filter filter_accel(cutoff_freq, sampling_time, order);
-const int scaling_factor = 100;
+const int scaling_factor = 1000;
 
 // Orientation tracking filter
-Madgwick filter;
-
-unsigned long microsPerReading, microsPrevious;
+SF fusion;
+float deltat;
 
 // Motion tracking
-float ax_old = 0.0f, ay_old = 0.0f, az_old = 0.0f; // needed to compute position (trapezoidal quadrature)
-float vx_old = 0.0f, vy_old = 0.0f, vz_old = 0.0f; // needed to compute position (trapezoidal quadrature)
-const float trans_thresh = 3.0; // threshold for motion = 3 m/s^2
+const float trans_thresh = 10.0; // threshold for motion = 3 m/s^2
+
+// Sensor data
+float ax = 0.0, ay = 0.0, az = 0.0; // accelerometer data
+float gx = 0.0, gy = 0.0, gz = 0.0; // gyroscope data
+float mx = 0.0, my = 0.0, mz = 0.0; // magnetometer data
 
 void setup() {
   Serial.begin(115200);
@@ -30,110 +32,89 @@ void setup() {
   }
   
   // Accelerometer code
+  IMU.accelUnit = METERPERSECOND2;
   IMU.setAccelFS(2);
   IMU.setAccelODR(5);
   IMU.setAccelOffset(-0.007029, -0.021880, -0.029258);
   IMU.setAccelSlope (1.003990, 0.994604, 1.006422);
 	
 	// Gyroscope code
+  IMU.gyroUnit = RADIANSPERSECOND;
   IMU.setGyroFS(2);
   IMU.setGyroODR(5);
   IMU.setGyroOffset (-0.546783, 1.013448, -1.081156);
   IMU.setGyroSlope (1.146890, 1.138180, 1.147532);
 
   // Magnetometer code
+  IMU.magnetUnit = NANOTESLA;
   IMU.setMagnetFS(1);
   IMU.setMagnetODR(8);
   IMU.setMagnetOffset(8.759766, 26.630859, -42.591553);
   IMU.setMagnetSlope (2.018031, 1.332266, 1.391874);
-
-  filter.begin(100.0f);
-
-  microsPerReading = 1000000 / 100;
-  microsPrevious = micros();
 }
 void loop() {
-  float ax, ay, az; // accelerometer data
-  float gx, gy, gz; // gyroscope data
-  float mx, my, mz; // magnetometer data
   float gravx, gravy, gravz; // gravity distribution on each axis
-  float sx, sy, sz; // motion
+  float sx = 0, sy = 0, sz = 0; // motion
   float vx, vy, vz; // velocity
-  float vx_raw, vy_raw, vz_raw;
-  float sx_raw, sy_raw, sz_raw;
   float roll, pitch, yaw; // rpy
-  unsigned long microsNow; // to compute elapsed time since last reading
 
-  microsNow = micros();
-  if (microsNow - microsPrevious >= microsPerReading) {
-    if (IMU.magnetAvailable()) {
-      IMU.readMagnet(mx, my, mz);
-    } else {
-      mx = 0.0;
-      my = 0.0;
-      mz = 0.0;
-    }
-    if (IMU.gyroscopeAvailable() && IMU.accelAvailable()) {
-      IMU.readGyroscope(gx, gy, gz);
-      IMU.readAccel(ax, ay, az);
-      
-      filter.update(gx, gy, gz, ax, ay, az, mx, my, mz);
-
-      roll = filter.getRollRadians();
-      pitch = filter.getPitchRadians();
-      yaw = filter.getYawRadians();
-
-      // Remove gravity component using the rotation matrix derived from rpy
-      gravx = cos(yaw) * sin(pitch) * cos(roll) + sin(yaw) * sin(roll);
-      gravy = sin(yaw) * sin(pitch) * cos(roll) - cos(yaw) * sin(roll);
-      gravz = cos(pitch) * cos(roll);
-
-      ax -= gravx;
-      ay -= gravy;
-      az -= gravz;
-
-      ax = filter_accel.filterIn(ax);
-      ay = filter_accel.filterIn(ay);
-      az = filter_accel.filterIn(az);
-      ax *= 9.81 * scaling_factor;
-      ay *= 9.81 * scaling_factor;
-      az *= 9.81 * scaling_factor;
-
-      // Only integrate if motion is bigger than threshold
-      int trans = ax*ax + ay*ay + az*az;
-      if (trans > trans_thresh) {
-        vx = (ax_old + ax) * sampling_time / 2;
-        vy = (ay_old + ay) * sampling_time / 2;
-        vz = (az_old + az) * sampling_time / 2;
-
-        sx = (vx_old + vx) * sampling_time / 2;
-        sy = (vy_old + vy) * sampling_time / 2;
-        sz = (vz_old + vz) * sampling_time / 2;
-
-        ax_old = ax;
-        ay_old = ay;
-        az_old = az;
-        vx_old = vx;
-        vy_old = vy;
-        vz_old = vz;
-      } else {
-        sx = 0;
-        sy = 0;
-        sz = 0;
-
-        ax_old = 0;
-        ay_old = 0;
-        az_old = 0;
-        vx_old = 0;
-        vy_old = 0;
-        vz_old = 0;
-      }
-
-      Serial.print(String(pitch) + ":" + String(roll) + ":" + String(yaw));
-      Serial.println(":" + String(sx) + ":" + String(sy) + ":" + String(sz));
-    }
-
-    microsPrevious = microsPrevious + microsPerReading;
+  if (IMU.magnetAvailable()) {
+    IMU.readMagnet(mx, my, mz);
+  } else {
+    mx = 0.0;
+    my = 0.0;
+    mz = 0.0;
   }
+
+  if (IMU.gyroscopeAvailable() && IMU.accelAvailable()) {
+    IMU.readGyroscope(gx, gy, gz);
+    IMU.readAccel(ax, ay, az);
+  }
+
+  deltat = fusion.deltatUpdate();
+
+  //fusion.MahonyUpdate(gx, gy, gz, ax, ay, az, mx, my, mz, deltat);
+  fusion.MadgwickUpdate(gx, gy, gz, ax, ay, az, mx, my, mz, deltat);
+  //fusion.MahonyUpdate(gx, gy, gz, ax, ay, az, deltat);
+  //fusion.MadgwickUpdate(gx, gy, gz, ax, ay, az, deltat);
+
+  roll = fusion.getRollRadians();
+  pitch = fusion.getPitchRadians();
+  yaw = fusion.getYawRadians();
+
+  float ax_temp, ay_temp, az_temp;
+
+  // Remove gravity component using the rotation matrix derived from rpy
+  ax_temp = ax * (cos(pitch) * cos(yaw)) + ay * (sin(roll) * sin(pitch) * cos(yaw) - cos(roll) * sin(yaw)) + az * (sin(roll) * sin(yaw) + cos(roll) * sin(pitch) * cos(yaw));
+  ay_temp = ax * (cos(pitch) * sin(yaw)) + ay * (cos(roll) * cos(yaw) + sin(roll) * sin(pitch) * sin(yaw)) + az * (cos(roll) * sin(pitch) * sin(yaw) - sin(roll) * cos(yaw));
+  az_temp = ax * (-sin(pitch)) + ay * (sin(roll) * cos(pitch)) + az * (cos(roll) * cos(pitch));
+
+  az_temp -= 9.81;
+
+  ax = ax_temp;
+  ay = ay_temp;
+  az = az_temp;
+
+  // ax = filter_accel.filterIn(ax);
+  // ay = filter_accel.filterIn(ay);
+  // az = filter_accel.filterIn(az);
+  ax *= scaling_factor;
+  ay *= scaling_factor;
+  az *= scaling_factor;
+
+  // Only integrate if motion is bigger than threshold
+  int trans = ax*ax + ay*ay + az*az;
+  if (trans > trans_thresh) {
+    vx = ax * deltat;
+    vy = ay * deltat;
+    vz = az * deltat;
+
+    sx = vx * deltat;
+    sy = vy * deltat;
+    sz = vz * deltat;
+  }
+
+  Serial.print(String(pitch) + ":" + String(roll) + ":" + String(yaw));
+  Serial.println(":" + String(sx) + ":" + String(sy) + ":" + String(sz));
 }
 
